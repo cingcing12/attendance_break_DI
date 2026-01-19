e699913b70c94bf5a9887a8a04ef29d35737e0d7
Vong sokpheak <vongsokpheak4@gmail.com>
2026-01-19 11:41:46 +0700

    changed!

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // 👇 ADD THIS LINE (Must match your repo name exactly!)
+  base: '/attendance_break_DI/', 
+  
   plugins: [
     vue(),
-    vueDevTools(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
